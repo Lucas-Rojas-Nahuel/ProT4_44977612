7f2588d347d11e4eb6f6ea29832ca3d13ea11ca3
LucasRojas <rolukasro12@gmail.com>
2024-08-07 17:31:35 -0300

    mostrando toda los libros de la lista

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import expess from 'express';
 import morgan from 'morgan';
+import { router } from './routes.js';
 
 const app = expess();
 
@@ -8,6 +9,8 @@ app.set('port', 3000);
 app.use(morgan('dev'));
 
 app.use(expess.json());
+
+app.use(router);
 
 app.listen(app.get('port'),()=>{
     console.log(`Server on port ${app.get('port')}`);
