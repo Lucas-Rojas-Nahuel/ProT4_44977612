c793a0e33ea1780d0671538834de925b164a2261
LucasRojas <rolukasro12@gmail.com>
2024-08-10 15:03:49 -0300

    punto A terminado, de obtener todas las lista y obtener libro por id

--- src/controller.js
+++ src/controller.js
@@ -1,10 +1,27 @@
-import {pool} from "./database.js";
+import { pool } from "./database.js";
 
-class LibroController{
+class LibroController {
 
-    async getAll(req, res){
-        const [result] = await pool.query('SELECT * FROM libros');
-        res.json(result);
+    async getAll(req, res) {
+        try {
+            const [result] = await pool.query('SELECT * FROM libros');
+            res.json(result);
+        } catch (err) {
+            res.json({ error: err.message });
+        }
+    }
+
+    async getOne(req, res){
+        const {id}= req.params;
+        try{
+            const [result] = await pool.query('SELECT * FROM libros WHERE id = ?', [id]);
+            if (result.length === 0){
+                return res.json({error: 'Libro no encontrado'});
+            }
+            res.json(result);
+        }catch(err){
+            res.json({error: err.message});
+        }
     }
 
 
