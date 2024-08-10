d2592fd8a2ef29199016b707c358819ebbb74ebb
LucasRojas <rolukasro12@gmail.com>
2024-08-10 17:22:32 -0300

    agregando archivo de base de datos a la carpeta script

--- src/index.js
+++ src/index.js
@@ -12,6 +12,15 @@ app.use(expess.json());
 
 app.use(router);
 
+//manejo de excepciones para solicitudes incompatibles
+app.use((err, req, res, next)=>{
+    if(err){
+        res.status(400).json({error: err.message});
+    } else {
+        next();
+    }
+});
+
 app.listen(app.get('port'),()=>{
     console.log(`Server on port ${app.get('port')}`);
 })
